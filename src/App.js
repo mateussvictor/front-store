d0a01e229493663978b81ce8d0d2846679911cc2
Mateus Victor <mateusvictor.dev@gmail.com>
2022-04-26 21:47:33 -0300

    chore: add eslint with airbnb code style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return (
-      <h1>Hello, Scandiweb</h1>
-    )
+    return <h1>Hello, Scandiweb</h1>;
   }
 }
 
-export { App }
+export default App;
